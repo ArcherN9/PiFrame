e7b8a36ab00c199974cafb0ea976bc899be84066
Daksh Srivastava <daksh_s@hcl.com>
2018-10-20 23:46:42 +0530

    Able to play videos albeit without sound and images from TCP port
    Signed-off-by: Daksh Srivastava <daksh_s@hcl.com>

--- node.lua
+++ node.lua
@@ -1,16 +1,12 @@
 -- Initializes the virtual screen of the current node. The variables NATIVE_WIDTH and NATIVE_HEIGHT
 -- contain the native screen width
-gl.setup(1024, 768)
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 -- Setup an image variable to hold the name of the image coming in from node js
-local visibleObject = resource.load_image "beamer.png"
+local visibleObject = ''
 local TYPE_IMAGE_1  = '.JPEG'
 local TYPE_IMAGE_2  = '.png'
 local TYPE_VIEO     = '.mp4'
-
-function readln()
-    return coroutine.yield()
-end
 
 if not N.clients then
     N.clients = {}
@@ -32,9 +28,7 @@ end)
 function echo(print)
     while true do
         -- Read a line of content coming in from nodejs server
-        -- local line = readln()
-        -- local line = "VID_20180204_192705.mp4"
-        local line = "beamer.png"
+        local line = readln()
         -- We know the line coming in is either an image name or a video. Save it in the visibleObject
         -- variable declared earlier.
         -- This image/video variable will now be used  in node.render()
@@ -42,32 +36,40 @@ function echo(print)
         -- Check if content is IMAGE
         if(string.find(line, TYPE_IMAGE_1) ~= nil)
             then
-                print("Detected Visual is an Image")
                 visibleObject = resource.load_image(line)
+                break
             end
 
         -- Check if content is IMAGE
         if(string.find(line, TYPE_IMAGE_2) ~= nil)
             then
-                print("Detected Visual is an Image")
                 visibleObject = resource.load_image(line)
+                break
             end
 
         -- Check if content is VIDEO
         if(string.find(line, TYPE_VIEO) ~= nil)
             then
                 visibleObject = resource.load_video {
-                    file = line;
-                    looped = false;
-                    audio = true;
-                    paused = false;
+                    file        = line;
+                    looped      = false;
+                    audio       = true;
+                    paused      = false;
                 }
+                break
             end
+
+        break
     end
+end
+
+function readln()
+    return coroutine.yield()
 end
 
 -- node.render is called for each frame so the moment a new image name comes in, it'll be replaced
 function node.render()
     gl.clear(1, 1, 1, 1)
-    util.draw_correct(visibleObject, 0, 0, WIDTH, HEIGHT)
+    -- util.draw_correct(visibleObject, 0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
+    visibleObject:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
 end
